a8234d19414e3f6d1891960791f7c9be0655a879
Richard Zhang <rszhang@usc.edu>
2025-09-11 11:27:52 +0000

    part 1 finished

--- split.cpp
+++ split.cpp
@@ -11,13 +11,55 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
-
+#include <cstddef>
 /* Add a prototype for a helper function here if you need */
+void walk(Node*& n, Node*& add);
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+  if (in == NULL){
+    return; // empty list
+  }
+  Node* next = in->next;
+  in->next = NULL; // detach (remove link of) next node from input before attaching to either odds or evens
+  if (in->value % 2 == 1){ // if odd
+    if (odds==NULL){
+      odds = in; // if odds list isn't initialized, set its head to the input
+    }
+    else{
+      Node* moving = odds; //creates a temporary pointer to walk the odds list
+      walk(moving, in); // helper function to traverse a list recursively
+    }
+  }
+  else if (in->value % 2 == 0){ // if even
+    if (evens==NULL){
+      evens = in; // if evens list isn't initialized, set its head to the input
+    }
+    else{
+      Node* moving = evens; //creates a temporary pointer to walk the evens list
+      walk(moving, in); // helper function to traverse a list recursively
+    }
+  }
+  split(next, odds, evens); // split the smaller input list
+  in = NULL; // sets the input to NULL as specified when the function terminates
 }
 
 /* If you needed a helper function, write it here */
+// recursively walks a singly linked list from head n
+// and adds node "add"
+void walk(Node*& n, Node*& add)
+{
+  if (n==NULL){
+    return;
+  }
+  if(n->next == NULL){// you have reached the end, so you can add this item
+    n->next = add;
+    return;
+  }
+  else {
+    walk(n->next, add); // traverses to the next node
+  }
+
+}
--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,55 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
-
+#include <cstddef>
+#include <iostream>
+void walk2(Node*& n, Node*& add);
+void print_list(Node*& head);
+void delete_list(Node*& head);
 int main(int argc, char* argv[])
 {
+  Node* start = new Node(1,NULL);
+  Node* temp = start;
+  Node* newNode = new Node(2, nullptr);
+  temp->next = newNode;
+  temp = newNode;
+  newNode = new Node(3, nullptr);
+  temp->next = newNode;
+  temp = newNode;
+  newNode = new Node(4, nullptr);
+  temp->next = newNode;
+  temp = newNode;
+  newNode = new Node(5, nullptr);
+  temp->next = newNode;
+  temp = newNode;
+  newNode = new Node(6, nullptr);
+  temp->next = newNode;
+  temp = newNode;
+  std::cout<<start->value<<std::endl;
+  std::cout<<start->next->next->value<<std::endl;
+  Node* odds = NULL;
+  Node* evens = NULL;
+  split(start, odds, evens);
+  std::cout<<odds->value<<std::endl;
+  std::cout<<odds->next->value<<std::endl;
+  std::cout<<odds->next->next->value<<std::endl;
+  std::cout<<evens->value<<std::endl;
+  std::cout<<evens->next->value<<std::endl;
+  std::cout<<evens->next->next->value<<std::endl;
 
+  delete_list(start);
+  delete_list(odds);
+  delete_list(evens);
+  delete odds;
+  delete evens;
+}
+void delete_list(Node*& head){
+  if (head==NULL){
+    return;
+  }
+  if (head->next == NULL){
+    return;
+  }
+  delete_list(head->next);
+  delete head->next;
 }
