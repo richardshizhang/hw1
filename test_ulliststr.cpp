b8817fe72667e296884431c633a4545f615479b4
Richard Zhang <rszhang@usc.edu>
2025-09-13 02:32:33 +0000

    part 2 finished

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -6,25 +6,96 @@
 #include "ulliststr.h"
 
 //Use this file to test your ulliststr implementation before running the test suite
-
+void check_empty(ULListStr& u);
+void check_back(ULListStr& u);
+void check_front(ULListStr& u);
 int main(int argc, char* argv[])
 {
-  ULListStr* u = new ULListStr();
-
-  std::string s = "1";
-  // u->push_back(s);
-  // u->pop_back();
-  // u->pop_back();
+  ULListStr u;
+  check_empty(u);
+  check_back(u);
+  check_front(u);
+}
+void check_empty(ULListStr& u){
+  //check empty cases
+  u.pop_back();
+  u.pop_front();
+  //add one value and empty twice
+  std::string s = "0";
+  u.push_back(s);
+  u.pop_back();
+  u.pop_front();
+  // check empty in middle
+  std::string s0 = "0";
+  u.push_back(s0);
+  std::string s1 = "1";
+  u.push_back(s1);
   std::string s2 = "2";
+  u.push_back(s2);
+  u.pop_front();
+  u.pop_back();
+  u.pop_back();
+  u.pop_front();
+  u.pop_back();
+}
+void check_back(ULListStr& u){
+  std::string s0 = "0";
+  u.push_back(s0);
+  std::string s1 = "1";
+  u.push_back(s1);
+  std::string s2 = "2";
+  u.push_back(s2);
   std::string s3 = "3";
-  u->push_back(s);
-  u->push_back(s2);
-  u->push_back(s3);
-  // std::cout<<u->size()<<std::endl;
-  // std::cout<<u->front()<<std::endl;
-  // std::cout<<u->back()<<std::endl;
-  std::cout<<u->get(0)<<std::endl;
-  // std::cout<<u->get(1)<<std::endl;
-  // std::cout<<u->get(2)<<std::endl;
-  delete u;
+  u.push_back(s3);
+  std::string s4 = "4";
+  u.push_back(s4);
+  std::string s5 = "5";
+  u.push_back(s5);
+  std::string s6 = "6";
+  u.push_back(s6);
+  std::string s7 = "7";
+  u.push_back(s7);
+  std::string s8 = "8";
+  u.push_back(s8);
+  std::string s9 = "9";
+  u.push_back(s9);
+  std::string s10 = "10";
+  u.push_back(s10);
+  std::cout<<u.size();
+  u.pop_back();
+  u.pop_back();
+  u.get(7);
+  u.back();
+  u.front();
+}
+void check_front(ULListStr& u){
+  std::string s0 = "0";
+  u.push_front(s0);
+  std::string s1 = "1";
+  u.push_front(s1);
+  std::string s2 = "2";
+  u.push_front(s2);
+  std::string s3 = "3";
+  u.push_front(s3);
+  std::string s4 = "4";
+  u.push_front(s4);
+  std::string s5 = "5";
+  u.push_front(s5);
+  std::string s6 = "6";
+  u.push_front(s6);
+  std::string s7 = "7";
+  u.push_front(s7);
+  std::string s8 = "8";
+  u.push_front(s8);
+  std::string s9 = "9";
+  u.push_front(s9);
+  std::string s10 = "10";
+  u.push_front(s10);
+  std::cout<<u.size();
+  u.pop_front();
+  u.pop_front();
+  u.get(7);
+  u.back();
+  u.front();
+
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -67,32 +67,36 @@ void ULListStr::clear()
 
 void ULListStr::push_back(const std::string& val)
 {
-  if (tail_ == NULL){
+  if (tail_ == NULL){ // empty list
     Item* newItem = new Item;
-    newItem->val[0] = val;
-    newItem->first = 0;
+    newItem->val[0] = val; //sets value at start of array
+    // set first and last and since it's the only node, prev and next as NULL
+    newItem->first = 0; 
     newItem->last = 1;
     newItem->prev = NULL;
     newItem->next = NULL;
+    // newItem is both the head and tail, and size is incremented
     head_ = newItem;
     tail_ = newItem;
     size_++;
     return;
   }
-  if (tail_->last==ARRSIZE){
+  if (tail_->last==ARRSIZE){ // if you need to add a new node
     Item* newItem = new Item;
-    newItem->val[0] = val;
-    newItem->prev = tail_;
-    newItem->next = NULL;
+    newItem->val[0] = val; // adds to front of new node's array
+    newItem->prev = tail_; // sets the previous to the old tail
+    newItem->next = NULL; // sets the next of the new tail to NULL
     newItem->first = 0;
     newItem->last = 1;
-    tail_->next = newItem;
+    // adds newItem to the tail and shifts the tail to the new one and increments size
+    tail_->next = newItem; 
     tail_ = newItem;
     size_++;
     return;
   }
   else{
-    tail_->val[tail_->last] = val;
+    // adds to the end and increments last and size
+    tail_->val[tail_->last] = val; 
     tail_->last++;
     size_++;
     return;
@@ -101,18 +105,18 @@ void ULListStr::push_back(const std::string& val)
 
 void ULListStr::pop_back()
 {
-  if (tail_==NULL){
+  if (tail_==NULL){ // empty list case
     return;
   }
-  if (tail_->last==1){
+  if (tail_->last==1){ // position of last element is the first position of an item
       Item* temp = tail_;
       tail_ = tail_->prev;
-      if (tail_ != NULL){
+      if (tail_ != NULL){ // if list isn't empty, new tail's next is set to NULL
         tail_->next = NULL;
       }
       delete temp;
       size_--;
-      if (size_==0){
+      if (size_==0){ // if empty list
         head_=NULL;
         tail_=NULL;
       }
@@ -132,31 +136,35 @@ void ULListStr::pop_back()
 
 void ULListStr::push_front(const std::string& val)
 {
-  if (head_ == NULL){
+  if (head_ == NULL){ // empty case
     Item* newItem = new Item;
-    newItem->val[0] = val;
+    // adds value to the front of the array and sets first, last, prev, next
+    newItem->val[0] = val; 
     newItem->first = 0;
     newItem->last = 1;
     newItem->prev = NULL;
     newItem->next = NULL;
+    // the new item is now both the head and tail, and size is incremented
     head_ = newItem;
     tail_ = newItem;
     size_++;
     return;
   }
-  if (head_->first==0){
+  if (head_->first==0){ // if we need to add a new node to the front
     Item* newItem = new Item;
+     // add the string to the last position of the new front node
     newItem->val[ARRSIZE-1] = val;
     newItem->next = head_;
     newItem->prev = NULL;
     newItem->first = ARRSIZE-1;
     newItem->last = ARRSIZE;
+    // shifts the old head to the new head, adding the prev link to the old head
     head_->prev = newItem;
     head_ = newItem;
     size_++;
     return;
   }
-  else{
+  else{ // adds to the front and moves first to the left
     head_->val[head_->first-1] = val;
     head_->first--;
     size_++;
@@ -165,26 +173,26 @@ void ULListStr::push_front(const std::string& val)
 
 void ULListStr::pop_front()
 {
-  if (head_==NULL){
+  if (head_==NULL){ // empty list case
     return;
   }
-  if (head_->first==ARRSIZE-1){
+  if (head_->first==ARRSIZE-1){ // if current first value is in the last position of a node
     Item* temp = head_;
     head_ = head_->next;
-    if (head_ != NULL){
+    if (head_ != NULL){// new head's prev value is now NULL
         head_->prev = NULL;
       }
     delete temp;
     size_--;
-    if (size_==0){
+    if (size_==0){//if list is fully empty
       head_=NULL;
       tail_=NULL;
     }
   }
   else{
-    head_->first++;
+    head_->first++;//move the value of first to the right
     size_--;
-    if (size_==0){ // if empty in the middle
+    if (size_==0){ // in case empty in the middle
       delete head_;
       head_=NULL;
       tail_=NULL;
@@ -195,11 +203,13 @@ void ULListStr::pop_front()
 
 std::string const & ULListStr::back() const
 {
-  return tail_->val[tail_->last-1];
+  // according to the video, we don't have to account for the empty list case
+  return tail_->val[tail_->last-1]; // value of the last item
 }
 std::string const & ULListStr::front() const
 {
-  return head_->val[head_->first];
+  // according to the video, we don't have to account for the empty list case
+  return head_->val[head_->first]; // value of the first item
 }
 /** 
    * Returns a pointer to the item at index, loc,
@@ -214,13 +224,16 @@ std::string* ULListStr::getValAtLoc(size_t loc) const
   if (head_==NULL){ // empty list
     return NULL;
   }
-  int modLoc = (loc%10+head_->first)%10; //the edited version that will have the index from 0 to ARRSIZE
+  int headfirst = head_->first; //shorthand mainly for the for loop condition
+  int modLoc = (loc%ARRSIZE+headfirst)%ARRSIZE; 
+  //loc modulo ARRSIZE shifted by the position of first in head that will have the index from 0 to ARRSIZE
   Item* temp = head_;
-  int headfirst = head_->first;
-  //std::cout<<loc-(ARRSIZE-headfirst)+1<<std::endl;
-
+  // walks the number of nodes necessary to get to the node that corresponds to loc
+  // ARRSIZE-headfirst is the amount of positions from the back where the first node starts
+  // increment by ARRSIZE to skip nodes to get closer to the proper node
+  // static casting because the condition may be negative but loc and arrsize are unsigned ints
   for (int i = 0; i<static_cast<int>(loc)-(static_cast<int>(ARRSIZE)-headfirst)+1;i+=ARRSIZE){
     temp = temp->next; // walks the number of nodes necessary
   }
-  return &temp->val[modLoc];
+  return &temp->val[modLoc];//once we're at the right node, we already know the position
 }
